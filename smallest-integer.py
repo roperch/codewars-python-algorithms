0ed1c31696ef457027dc69d4266e8a05c51583cd
Christopher H Roper <roperch@optusnet.com.au>
2018-04-29 13:17:31 +1000

    Find the smallest integer in the array
    Given an array of integers your solution should find the smallest integer.
    
    For example:
    
    Given [34, 15, 88, 2] your solution will return 2
    Given [34, -345, -1, 100] your solution will return -345
    You can assume, for the purpose of this kata, that the supplied array will not be empty.

---
+++ smallest-integer.py
@@ -0,0 +1,7 @@
+# test cases:
+# find_smallest_int([78, 56, 232, 12, 11, 43]) --> 11
+# find_smallest_int([78, 56, -2, 12, 8, -33]) --> -33
+# find_smallest_int([0, 1-2**64, 2**64]) --> 1-2**64
+
+def find_smallest_int(arr):
+    return min(arr);
